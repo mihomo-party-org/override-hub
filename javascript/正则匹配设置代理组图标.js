7b3d5aea425afc2a1d7d92c82c374ff43e791923
布丁狗~ <pompurin404@mihomo.party>
2024-10-07 19:10:38 +0800

    Create 正则匹配设置代理组图标.js

---
+++ javascript/正则匹配设置代理组图标.js
@@ -0,0 +1,22 @@
+const iconMap = [
+  {
+    reg: /Google/,
+    url: "https://fastly.jsdelivr.net/gh/Koolson/Qure@master/IconSet/Color/Google_Search.png",
+  },
+  // 参考上面格式在这里添加更多条目
+];
+const keepOrigin = true; // 是否保留配置中原本存在的图标
+
+function main(config) {
+  if (!config["proxy-groups"]) return config;
+  for (let group of config["proxy-groups"]) {
+    if (keepOrigin && group.icon) continue;
+    for (let { reg, url } of iconMap) {
+      if (group.name.match(reg)) {
+        group.icon = url;
+        break;
+      }
+    }
+  }
+  return config;
+}
