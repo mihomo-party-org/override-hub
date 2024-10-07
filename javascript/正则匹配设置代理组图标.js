4196a9070a2e03babc4b85b0d440f0551b51eb8d
布丁狗~ <pompurin404@mihomo.party>
2024-10-07 19:12:35 +0800

    Update 正则匹配设置代理组图标.js

--- javascript/正则匹配设置代理组图标.js
+++ javascript/正则匹配设置代理组图标.js
@@ -1,9 +1,11 @@
+// 正则匹配设置代理组图标
+
 const iconMap = [
   {
     reg: /Google/,
     url: "https://fastly.jsdelivr.net/gh/Koolson/Qure@master/IconSet/Color/Google_Search.png",
   },
-  // 参考上面格式在这里添加更多条目
+  // 参考上面的格式在这里添加更多条目
 ];
 const keepOrigin = true; // 是否保留配置中原本存在的图标
 
